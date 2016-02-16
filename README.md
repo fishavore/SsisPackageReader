f70fa5ae047770ecc19244e6223bca52c89e1949
fishavore <fishavore@users.noreply.github.com>
2016-02-15 21:01:45 -0800

    Updated ReadMe.md and re-uploaded files

--- README.md
+++ README.md
@@ -6,11 +6,19 @@ Document SSIS package content.
 
 ## Revision history
 
+- 2016-02-15 Updated ReadMe.md and re-uploaded files.
 - 2016-01-12 Created this GitHub repo and uploaded files.
 
 ## Description
 
-SSIS Package Reader provides free-form entry of a search term and returns all entries that contain that text.
+SSIS Package Reader provides free-form entry of a search term and returns all entries in the supported tasks that contain that text.  
+  
+Supported tasks are:
+
+- Data Flow Task
+  - Destination
+  - Source
+- Execute SQL Task
 
 **Note:** This project was completed in a private Bitbucket repository.
 
@@ -24,10 +32,3 @@ SSIS Package Reader provides free-form entry of a search term and returns all en
   - Required assemblies: Microsoft.SQLServer.ManagedDTS.dll (Microsoft.SqlServer.Dts.Runtime Namespace) and Microsoft.SqlServer.SQLTask.dll (Microsoft.SqlServer.Dts.Tasks.ExecuteSQLTask Namespace)
 
 **Note:** Other references are given in the source code.
-
-## Files
-
-- ContainerType.cs: Enumerator used by ReadingSsisPackage.cs
-- MainForm.cs: User interface
-- ReadingSsisPackage.cs: ReadingSsisPackage class
-- SsisPackageReader.exe: Windows Forms application
---
+++ SsisPackageReader/SsisPackageReader/MainForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace SsisPackageReader {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tableNameLabel = new System.Windows.Forms.Label();
+            this.resultsLabel = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.resultsRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dashToolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dashToolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.progressToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.searchTermTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchAllButton = new System.Windows.Forms.Button();
+            this.mainMenuStrip.SuspendLayout();
+            this.mainStatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableNameLabel
+            // 
+            this.tableNameLabel.AutoSize = true;
+            this.tableNameLabel.Location = new System.Drawing.Point(12, 47);
+            this.tableNameLabel.Name = "tableNameLabel";
+            this.tableNameLabel.Size = new System.Drawing.Size(73, 15);
+            this.tableNameLabel.TabIndex = 0;
+            this.tableNameLabel.Text = "Search Term";
+            // 
+            // resultsLabel
+            // 
+            this.resultsLabel.AutoSize = true;
+            this.resultsLabel.Location = new System.Drawing.Point(12, 83);
+            this.resultsLabel.Name = "resultsLabel";
+            this.resultsLabel.Size = new System.Drawing.Size(44, 15);
+            this.resultsLabel.TabIndex = 2;
+            this.resultsLabel.Text = "Results";
+            // 
+            // resultsRichTextBox
+            // 
+            this.resultsRichTextBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.resultsRichTextBox.Location = new System.Drawing.Point(12, 109);
+            this.resultsRichTextBox.Name = "resultsRichTextBox";
+            this.resultsRichTextBox.ReadOnly = true;
+            this.resultsRichTextBox.Size = new System.Drawing.Size(759, 424);
+            this.resultsRichTextBox.TabIndex = 6;
+            this.resultsRichTextBox.Text = "";
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.BackColor = System.Drawing.Color.Lavender;
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(784, 24);
+            this.mainMenuStrip.TabIndex = 7;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.dashToolStripMenuItem1,
+            this.resetToolStripMenuItem,
+            this.dashToolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            // 
+            // dashToolStripMenuItem1
+            // 
+            this.dashToolStripMenuItem1.Name = "dashToolStripMenuItem1";
+            this.dashToolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.resetToolStripMenuItem.Text = "&Reset";
+            // 
+            // dashToolStripMenuItem2
+            // 
+            this.dashToolStripMenuItem2.Name = "dashToolStripMenuItem2";
+            this.dashToolStripMenuItem2.Size = new System.Drawing.Size(100, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // mainStatusStrip
+            // 
+            this.mainStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressToolStripStatusLabel});
+            this.mainStatusStrip.Location = new System.Drawing.Point(0, 540);
+            this.mainStatusStrip.Name = "mainStatusStrip";
+            this.mainStatusStrip.Size = new System.Drawing.Size(784, 22);
+            this.mainStatusStrip.TabIndex = 8;
+            // 
+            // progressToolStripStatusLabel
+            // 
+            this.progressToolStripStatusLabel.Name = "progressToolStripStatusLabel";
+            this.progressToolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // searchTermTextBox
+            // 
+            this.searchTermTextBox.Location = new System.Drawing.Point(89, 44);
+            this.searchTermTextBox.Name = "searchTermTextBox";
+            this.searchTermTextBox.Size = new System.Drawing.Size(400, 23);
+            this.searchTermTextBox.TabIndex = 9;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(501, 43);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 10;
+            this.searchButton.Text = "&Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // searchAllButton
+            // 
+            this.searchAllButton.Location = new System.Drawing.Point(588, 43);
+            this.searchAllButton.Name = "searchAllButton";
+            this.searchAllButton.Size = new System.Drawing.Size(75, 23);
+            this.searchAllButton.TabIndex = 11;
+            this.searchAllButton.Text = "Search &All";
+            this.searchAllButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.searchAllButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTermTextBox);
+            this.Controls.Add(this.mainStatusStrip);
+            this.Controls.Add(this.resultsRichTextBox);
+            this.Controls.Add(this.resultsLabel);
+            this.Controls.Add(this.tableNameLabel);
+            this.Controls.Add(this.mainMenuStrip);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SSIS Package Reader";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.mainStatusStrip.ResumeLayout(false);
+            this.mainStatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tableNameLabel;
+        private System.Windows.Forms.Label resultsLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.RichTextBox resultsRichTextBox;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator dashToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip mainStatusStrip;
+        private System.Windows.Forms.ToolStripSeparator dashToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel progressToolStripStatusLabel;
+        private System.Windows.Forms.TextBox searchTermTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button searchAllButton;
+    }
+}
+
